c497c05779a9993d9c791339eb2ab1bb26acccde
Mia Borja <apk.borja@gmail.com>
2022-03-06 20:19:19 -0800

    Added dialog for button click.

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    var lengthCheck = prompt("How many characters would you like?"); {
+      if (lengthCheck.isInteger === true) {
+        
+      } else {
+        alert("Please enter a real number.")
+      }
+    }
+    var symbolCheck = confirm("Would you like special characters?");
+    var uppercaseCheck = confirm("Would you like uppercase characters?");
+    var lowercaseCheck = confirm("Would you like lowercase characters?");
+    var numberCheck = confirm("Would you like numbers?");
+  return;
+}
